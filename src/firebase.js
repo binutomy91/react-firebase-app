fe6f5802a4812831580dfa98f3eeaebb3e059c7c
btomy <binu.tomy@citizensadvice.org.uk>
2018-10-04 11:29:35 +0100

    setup [adding folder]

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+  // Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCAvJeA1fGWaU7I-Ui3Io6n1AJsA_el1FI",
+    authDomain: "sample-project-7c313.firebaseapp.com",
+    databaseURL: "https://sample-project-7c313.firebaseio.com",
+    projectId: "sample-project-7c313",
+    storageBucket: "sample-project-7c313.appspot.com",
+    messagingSenderId: "185912069847"
+};
+
+firebase.initializeApp(config);
+export default firebase;
